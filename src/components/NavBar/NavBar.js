090d55551cced892248fa320c51955a5730e9662
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-03-04 21:38:55 -0800

    create navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { Transition } from "@headlessui/react";
+import Logo from "./components/Logo";
+import DesktopNav from "./components/DesktopNav";
+import MobileNav from "./components/MobileNav";
+
+function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div>
+      <nav className="fixed top-0 w-full bg-white z-50">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <Logo />
+              <DesktopNav />
+            </div>
+            <MobileNav setIsOpen={setIsOpen} isOpen={isOpen} />
+          </div>
+        </div>
+
+        <Transition
+          show={isOpen}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          {(ref) => (
+            <div className="md:hidden" id="mobile-menu">
+              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                <a
+                  href="#"
+                  className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Dashboard
+                </a>
+
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Team
+                </a>
+
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Projects
+                </a>
+
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Calendar
+                </a>
+
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Reports
+                </a>
+              </div>
+            </div>
+          )}
+        </Transition>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
