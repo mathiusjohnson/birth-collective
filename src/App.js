f71492051963b369ef794f1415429907456b17df
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-03-04 21:38:32 -0800

    add nav and parallax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Nav from "./components/NavBar/NavBar";
+import Parallax from "./components/Parallax/Parallax";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="relative">
+      <Nav />
+      <Parallax />
+    </div>
+  )
 }
 
 export default App;
